bbf6c36b41ee05f7655d0d64b65cab2f8efd7615
Michael Evans <tcs_mevans@ps355701.dreamhostps.com>
2015-04-12 14:26:50 -0700

    Words have been added

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,3 +1,36 @@
-getbootstrap.com
+<!--getbootstrap.com
 css media query:
-    style sheet knows the browers
+    style sheet knows the browers-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Mike's Repostage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <style>
+h1 {
+    font-size: 50px;
+}
+p {
+    font-size: 20px;
+}
+div {
+    height: 3000px;
+    left:10%;
+    margin-top:225px;
+    position: fixed;
+    margin-left: -200px;
+}
+</style>
+</head>
+<body>
+    <div class="container-fluid">
+        <h1>Print & Ship Photos</h1>
+        <p>Add #Repostage to any of your Instagram photos and get a printed postcard in the mail.</p>
+    </div>
+</body>
+</html>
