b493742e9dfdb67408bc270bde4785a2f308241b
Michael Evans <tcs_mevans@ps355701.dreamhostps.com>
2015-04-16 22:03:40 -0700

    More words are there

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -12,25 +12,53 @@ css media query:
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <style>
-h1 {
-    font-size: 50px;
-}
-p {
-    font-size: 20px;
-}
-div {
-    height: 3000px;
-    left:10%;
-    margin-top:225px;
-    position: fixed;
-    margin-left: -200px;
-}
-</style>
+        body{
+            font-family: sans-serif;
+            line-height:1;
+        }
+        .header{
+            position: relative;
+            height: 0px;
+        }
+        .hero {
+            padding: 15.625rem 0; }
+        .hero h1 {
+            margin: 0 0 0.8125rem 0;}
+        .hero h3 {
+            margin: 0 0 1.875rem 0;}
+     </style>
 </head>
 <body>
+<div class="header">
     <div class="container-fluid">
-        <h1>Print & Ship Photos</h1>
-        <p>Add #Repostage to any of your Instagram photos and get a printed postcard in the mail.</p>
+        <div class="row">
+            <div class="col-xs-10"></div>
+            <div class="col-xs-2">
+                <div class="log in">
+                    <span>
+                        <a href="http://i.huffpost.com/gen/1548397/images/o-NIC-CAGE-facebook.jpg">Log in
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+<div class="hero" style='background-image: nic-cage.jpg; background-size: cover; background-position: center center; background-repeat: no-repeat;'>
+   <div class="container-fluid">
+     <div class="row">
+       <div class="col-sm-6">
+         <h1>Print &amp; Ship Photos</h1>
+         <h3>Add #Repostage to any of your Instagram photos and get a printed postcard in the mail.</h3>
+         <a href="http://i.huffpost.com/gen/1548397/images/o-NIC-CAGE-facebook.jpg">
+           <div class="button text-center">
+             <span>Nic Cage</span>
+           </div>
+         </a>
+       </div>
+     </div>
+   </div>
+ </div>
+        
 </body>
 </html>
