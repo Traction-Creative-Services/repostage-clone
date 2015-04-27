91d8cb6c82a098ae6ac0961c22dc1b4f8d6cfb61
tcs-mevans <mike@traction.media>
2015-04-27 18:20:08 -0500

    added logo on top left, fixed picture in middle, fixed pictures at bottom

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -21,10 +21,33 @@ css media query:
         }
         a{
           color: #FFF;
-          rext-decoration: none;
+          text-decoration: none;
         }
         .header{
-            height: 0px;
+          position: relative;
+            height: 0;
+            color: #FFF;
+        }
+        .header .logo{
+          position: relative;
+          top: 50%
+          transform: translateY(-50%);
+          height: 2.75rem;
+          width: 2.75rem;
+          background-image: url("logo.svg");
+          background-size: contain;
+          float: left;
+        }
+        .header .email{
+          position: relative;
+          top: 50%
+          transform: translateY(-50%);
+          float: right;
+        }
+        .header .email span{
+          font-size: 0.875rem;
+          line-height: 1.1875rem;
+          letter-spacing: 0.03125rem;
         }
         .hero {
             padding: 9.375rem 0px;
@@ -36,14 +59,37 @@ css media query:
             background-repeat: no-repeat;
         }
         .setup{
-              padding: 9.375rem 0;
+          color: #505050;
+          padding: 9.375rem 0;
+        }
+        .setup .steps-container{
+          margin: 3.125rem;
         }
         .setup .step{
-            padding: 0px;
+            display: flex;
+            padding: 0 2.5rem;
+        }
+        .setup .step .step-number{
+          margin:0 0.625rem 0 0;
+          display:table;
+          font-size:3.125rem;
+        }
+        .setup .step .step-content{
+          display:table-cell;
+        }
+        .setup .step .step-content h4{
+          font-weight: 700;
+          margin-bottom:0.125rem;
         }
         .pay {
-            color: rgb(255, 255, 255);
+            color: #FFF;
             background-image: radial-gradient(at left top , #3A8FDC 0%, #87D7CD 100%);
+        }
+        .pay .cards-mobile-container{
+          display: none;
+        }
+        @media(max-width:786px) .pay .cards-mobile-container{
+          display: inline-block;
         }
         .stripe{
             height: 1.4375rem;
@@ -56,6 +102,10 @@ css media query:
             /*margin: 0.625rem auto 0px;*/
             height: 2.375rem;
             background: url("http://www.repostage.com/static/card-icons.png") no-repeat scroll center center / contain transparent;            
+        }
+        .table{
+          padding: 5rem 0;
+          color: #505050;
         }
         .end {
           color: #FFF;
@@ -124,9 +174,13 @@ css media query:
     <div class="header">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-xs-10"></div>
-                <div class="col-xs-2">
-                  <a href="http://i.huffpost.com/gen/1548397/images/o-NIC-CAGE-facebook.jpg" class="btn btn-primary" id="log-in-btn">Log in</a>
+                <div class="col-sm-12">
+                  <a href="/"><div class="logo"></div></a>
+                  <div class="email">
+                    <span>
+                      <a href="http://i.huffpost.com/gen/1548397/images/o-NIC-CAGE-facebook.jpg">Log in</a>
+                    </span>
+                  </div>
                 </div>
             </div>
         </div>
@@ -154,7 +208,7 @@ css media query:
                     <h5 class="text-center">Signing up for Repostage is fast and free. Pay only when you use it.</h5>
                     <div class="steps-container">
                         <div class="row">
-                            <div class="step col-xs-4">
+                            <div class="step col-sm-4">
                                 <div class="step-number">
                                     <h1>1</h1>
                                 </div>
@@ -163,7 +217,7 @@ css media query:
                                     <p>Sign in with your Instagram account one time to create and link your Repostage account.</p>
                                 </div>
                             </div>
-                            <div class="step col-xs-4">
+                            <div class="step col-sm-4">
                                 <div class="step-number">
                                     <h1>2</h1>
                                 </div>
@@ -172,11 +226,11 @@ css media query:
                                     <p>With your shipping and billing information entered, you’re good to go. Start enjoying step 3.</p>
                                 </div>
                             </div>
-                            <div class="step col-xs-4">
+                            <div class="step col-sm-4">
                                 <div class="step-number">
                                     <h1>3</h1>
                                 </div>
-                                <div class="steps-content">
+                                <div class="step-content">
                                     <h4>Comment and Enjoy!</h4>
                                     <p>Add #Repostage to any of your photos (new and old) and we’ll print it within the next 24 hours.</p>
                                 </div>
@@ -190,9 +244,9 @@ css media query:
     <div class="pay">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-xs-12">
+                <div class="col-sm-12">
                     <div class="row">
-                        <div class="col-xs-6">
+                        <div class="col-sm-6 cards-mobile-container">
                             <div class="pic">
                                 <img class="img-responsive" src="http://replygif.net/thumbnail/1119.gif"></img>
                             </div>
@@ -214,6 +268,9 @@ css media query:
                                 <div class="btn btn-primary">Sign up with</div>
                             </div>
                         </div>
+                        <div class="col-md-4 cards-container">
+                          <img class="img-responsive" src="http://replygif.net/thumbnail/1119.gif"></img>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -222,35 +279,35 @@ css media query:
     <div class="table">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-xs-12 text-center">
+          <div class="col-sm-12 text-center">
             <h2><strong>#Repostage</strong> In Action</h2>
             <h5>See, it works! Below are the most recent awesome photos being printed right now.</h5>
             <div class="row photo-row">
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://1.bp.blogspot.com/-xEeb9swx1ks/UP1vlgRW-rI/AAAAAAAAC8Y/wexMZkkwBLs/s1600/Chris-Shen-cage-michael-jackson-590x350.jpg"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://1.bp.blogspot.com/-xEeb9swx1ks/UP1vlgRW-rI/AAAAAAAAC8Y/wexMZkkwBLs/s1600/Chris-Shen-cage-michael-jackson-590x350.jpg"></img>
               </div>
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://d4f1ndlb0hkjb.cloudfront.net/wp-content/uploads/2010/01/nick-cage-abe-lincoln.jpg"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://cdn.morefm.co.nz/morefm/AM/2013/2/21/6075/Nicolas-Cage-Face-20-Mona-Lisa.jpg"></img>
               </div>
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://unrealitymag.bcmediagroup.netdna-cdn.com/wp-content/uploads/2010/01/nicholas_cage_12.jpg"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://unrealitymag.bcmediagroup.netdna-cdn.com/wp-content/uploads/2010/01/nicholas_cage_12.jpg"></img>
               </div>
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://media.tumblr.com/tumblr_m5yl9uOHhF1qmwrao.png"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://nicolascage.us/wp-content/uploads/2013/09/Something-About-Mary.jpg"></img>
               </div>
             </div>
             <div class="row photo-row">
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://cdn.morefm.co.nz/morefm/AM/2013/2/21/6075/Nicolas-Cage-Face-20-Mona-Lisa.jpg"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://d4f1ndlb0hkjb.cloudfront.net/wp-content/uploads/2010/01/nick-cage-abe-lincoln.jpg"></img>
               </div>
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="https://aplaceforpeace.files.wordpress.com/2010/02/dylanreevenic-clinton.jpg"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="https://aplaceforpeace.files.wordpress.com/2010/02/dylanreevenic-clinton.jpg"></img>
               </div>
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://media.tumblr.com/8876a5b97d45be5811442d8ea25d936f/tumblr_inline_mhhtb3G0w41qz4rgp.png"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://media.tumblr.com/8876a5b97d45be5811442d8ea25d936f/tumblr_inline_mhhtb3G0w41qz4rgp.png"></img>
               </div>
               <div class="col-sm-3 col-xs-6">
-                <img class="img-responsive" alt="In Action" src="http://nicolascage.us/wp-content/uploads/2013/09/Something-About-Mary.jpg"></img>
+                <img class="img-responsive img-rounded" alt="In Action" src="http://media.tumblr.com/tumblr_m5yl9uOHhF1qmwrao.png"></img>
               </div>
             </div>
           </div>
@@ -260,7 +317,7 @@ css media query:
     <div class="end">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-xs-12 text-center">
+          <div class="col-sm-12 text-center">
             <h1>Does this all sound good?</h1>
             <h5>Well it's all true. Start enjoying Repostage today.</h5>
             <div class="btn btn-primary">Sign up with</div>
@@ -271,16 +328,16 @@ css media query:
     <div class="footer">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-xs-5 text-left">
+          <div class="col-sm-5 text-left">
             <span>Made in Orange County</span>
             <div class="california"></div>
           </div>
-          <div class="col-xs-2 text-center">
+          <div class="col-sm-2 text-center">
             <a href="http://vodzilla.co/wp-content/uploads/2013/08/con-air-nic-cage-700x325.jpg" class="instagram"></a>
             <a href="http://25.media.tumblr.com/tumblr_m8wvscKuTy1rcgim4o1_400.jpg" class="facebook"></a>
             <a href="http://bumbumbum.me/wp-content/uploads/2010/01/Chironex-Fleckerinickywonka-e1263934224121.jpg" class="twitter"></a>
           </div>
-          <div class="col-xs-5 text-right">
+          <div class="col-sm-5 text-right">
             <ul>
               <li><span><a href="/joe">FAQ</a></span></li>
               <li><span><a href="/joe">Legal</a></span></li>
